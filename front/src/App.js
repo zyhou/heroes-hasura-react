22faa8c63a493522c9a36b48ab5e3ad83e919c31
zyhou <richard.maxime9@gmail.com>
2018-12-08 23:22:18 +0100

    bootstrap react-admin

--- front/src/App.js
+++ front/src/App.js
@@ -1,26 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import buildGraphQLProvider from "ra-data-graphql";
+import { Admin, Resource, ListGuesser } from "react-admin";
+import { ApolloLink } from "apollo-link";
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+
+const client = new ApolloClient({
+  link: new HttpLink({ uri: "http://localhost:8080/v1alpha1/graphql" }),
+  cache: new InMemoryCache()
+});
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { dataProvider: null };
+  }
+
+  componentDidMount() {
+    buildGraphQLProvider({ client }).then(dataProvider =>
+      this.setState({ dataProvider })
+    );
+  }
+
   render() {
+    const { dataProvider } = this.state;
+
+    if (!dataProvider) {
+      return <div>Loading</div>;
+    }
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Admin dataProvider={dataProvider}>
+        <Resource name="heroes" list={ListGuesser} />
+      </Admin>
     );
   }
 }
