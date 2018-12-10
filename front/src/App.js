bfb07c6b5f7c69f6d3163651ea8fea1506b8d8a2
zyhou <richard.maxime9@gmail.com>
2018-12-10 18:05:21 +0100

    list heroes + query apollo

--- front/src/App.js
+++ front/src/App.js
@@ -1,74 +1,34 @@
 import React from "react";
-
-import ApolloClient from "apollo-boost";
 import gql from "graphql-tag";
-import { ApolloProvider } from "react-apollo";
+import { Query } from "react-apollo";
 
-const client = new ApolloClient({
-  uri: "http://localhost:8080/v1alpha1/graphql"
-});
-
-client
-  .query({
-    query: gql`
-      {
-        heroes {
-          name
-          description
-        }
-      }
-    `
-  })
-  .then(result => console.log(result));
+const GET_HEROES = gql`
+  {
+    heroes {
+      id
+      name
+      description
+    }
+  }
+`;
 
 const App = () => (
-  <ApolloProvider client={client}>
-    <div>
-      <h2>My first Apollo app 🚀</h2>
-    </div>
-  </ApolloProvider>
+  <Query query={GET_HEROES}>
+    {({ loading, error, data }) => {
+      if (loading) return "Loading...";
+      if (error) return `Error! ${error.message}`;
+
+      return (
+        <ul>
+          {data.heroes.map(heroe => (
+            <li key={heroe.id} value={heroe.name}>
+              {heroe.name}
+            </li>
+          ))}
+        </ul>
+      );
+    }}
+  </Query>
 );
 
 export default App;
-
-// import React, { Component } from "react";
-// import buildGraphQLProvider from "ra-data-graphql";
-// import { Admin, Resource, ListGuesser } from "react-admin";
-// import { ApolloLink } from "apollo-link";
-// import { ApolloClient } from "apollo-client";
-// import { InMemoryCache } from "apollo-cache-inmemory";
-// import { HttpLink } from "apollo-link-http";
-
-// const client = new ApolloClient({
-//   link: new HttpLink({ uri: "http://localhost:8080/v1alpha1/graphql" }),
-//   cache: new InMemoryCache()
-// });
-
-// class App extends Component {
-//   constructor() {
-//     super();
-//     this.state = { dataProvider: null };
-//   }
-
-//   componentDidMount() {
-//     buildGraphQLProvider({ client }).then(dataProvider =>
-//       this.setState({ dataProvider })
-//     );
-//   }
-
-//   render() {
-//     const { dataProvider } = this.state;
-
-//     if (!dataProvider) {
-//       return <div>Loading</div>;
-//     }
-
-//     return (
-//       <Admin dataProvider={dataProvider}>
-//         <Resource name="heroes" list={ListGuesser} />
-//       </Admin>
-//     );
-//   }
-// }
-
-// export default App;
--- front/src/index.js
+++ front/src/index.js
@@ -1,9 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const client = new ApolloClient({
+  uri: "http://localhost:8080/v1alpha1/graphql"
+});
+
+const ConnectedApp = () => (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
+
+ReactDOM.render(<ConnectedApp />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
